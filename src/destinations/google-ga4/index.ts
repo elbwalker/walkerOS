c861a9b5973aa1d9e2a643015305d668f3865c44
alexander <Alexander@Kirtzel.com>
2022-04-06 00:47:33 +0200

    ga4 setup

---
+++ src/destinations/google-ga4/index.ts
@@ -0,0 +1,56 @@
+import { AnyObject, Elbwalker, WebDestination } from '@elbwalker/types';
+
+declare global {
+  interface Window {
+    dataLayer?: unknown[];
+    gtag: Function;
+  }
+}
+
+export interface DestinationGA4 extends WebDestination.Function {
+  config: WebDestination.Config & {
+    measurementId?: string;
+    transport_url?: string;
+  };
+}
+
+const w = window;
+let measurementId: string;
+
+export const destination: DestinationGA4 = {
+  config: {},
+
+  init() {
+    let config = this.config;
+    const settings: AnyObject = {};
+
+    // required measuremt id
+    if (!config.measurementId) return false;
+    measurementId = config.measurementId;
+
+    // custom transport url
+    if (config.transport_url) settings.transport_url = config.transport_url;
+
+    // setup required methods
+    w.dataLayer = w.dataLayer || [];
+    if (!w.gtag) {
+      w.gtag = function gtag() {
+        w.dataLayer!.push(arguments);
+      };
+      w.gtag('js', 's');
+      // w.gtag('js', new Date());
+    }
+
+    // gtag init call
+    w.gtag('config', measurementId, settings);
+
+    return true;
+  },
+
+  push(event: Elbwalker.Event) {
+    let data = event.data || {};
+    data.send_to = measurementId;
+
+    w.gtag('event', `${event.entity} ${event.action}`, data);
+  },
+};
