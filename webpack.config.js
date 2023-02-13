58e8b85d7e8e15d702f38553ea6da735cb337bf7
alexander <Alexander@Kirtzel.com>
2023-02-13 10:16:22 +0100

    walkers esm build

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,34 @@ const browserConfig = {
   plugins: [],
 };
 
+const moduleConfig = {
+  mode: 'production',
+  entry: './src/modules/node.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  experiments: {
+    outputModule: true,
+  },
+  output: {
+    library: {
+      type: 'module',
+    },
+    filename: 'index.mjs',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [],
+};
+
 const utilsConfig = {
   mode: 'production',
   entry: './src/lib/utils.ts',
@@ -73,4 +101,4 @@ const utilsConfig = {
   plugins: [],
 };
 
-module.exports = [browserConfig, nodeConfig, utilsConfig];
+module.exports = [browserConfig, nodeConfig, moduleConfig, utilsConfig];
