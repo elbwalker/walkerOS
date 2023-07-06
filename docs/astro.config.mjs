68c32b0a22c1162d231030bc9902212d9f288e83
alexander <Alexander@Kirtzel.com>
2023-07-06 15:41:32 +0200

    hello docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: 'walker.js docs',
+      customCss: ['/src/styles/custom.css'],
+      logo: {
+        src: '/src/styles/favicon.svg',
+      },
+      social: {
+        github: 'https://github.com/elbwalker/walker.js',
+      },
+      sidebar: [
+        {
+          label: 'Introduction',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', link: '/introduction/basic-idea/' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+
+  // Process images with sharp: https://docs.astro.build/en/guides/assets/#using-sharp
+  image: { service: { entrypoint: 'astro/assets/services/sharp' } },
+});
