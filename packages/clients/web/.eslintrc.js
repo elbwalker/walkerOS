1a25e6a4efa40e12ed1e4ec67a7e64b4a2faaf7d
alexander <Alexander@Kirtzel.com>
2023-08-16 19:17:23 +0200

    turborepo

--- packages/clients/web/.eslintrc.js
+++ packages/clients/web/.eslintrc.js
@@ -1,27 +1,9 @@
 module.exports = {
   root: true,
+  extends: ["custom"],
   env: {
-    es6: true,
     browser: true,
+    es6: true,
     jest: true,
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jest'],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:jest/recommended',
-    'prettier',
-  ],
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {
-    'no-console': 'warn',
-    'jest/no-disabled-tests': 'warn',
-    'jest/no-focused-tests': 'error',
   },
 };
