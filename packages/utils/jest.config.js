f69394b847a445ac4359cba41c6824030b19e11f
alexander <Alexander@Kirtzel.com>
2023-08-30 11:56:00 +0200

    test env node as default

--- packages/utils/jest.config.js
+++ packages/utils/jest.config.js
@@ -1,5 +1,7 @@
 const commonConfig = require('@elbwalker/jest');
 
-const config = {};
+const config = {
+  testEnvironment: 'jsdom', // @TODO edit for node utils
+};
 
 module.exports = { ...commonConfig, ...config };
