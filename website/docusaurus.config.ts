e0ffc2562c93b676002f043e8f63e056a8b823b9
alexander <Alexander@Kirtzel.com>
2023-11-24 18:04:12 +0100

    blog style

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -134,19 +134,19 @@ const config: Config = {
     },
   } satisfies Preset.ThemeConfig,
 
-  plugins: [
-    async function myPlugin(context, options) {
-      return {
-        name: 'docusaurus-tailwindcss',
-        configurePostCss(postcssOptions) {
-          // Appends TailwindCSS and AutoPrefixer.
-          postcssOptions.plugins.push(require('tailwindcss'));
-          postcssOptions.plugins.push(require('autoprefixer'));
-          return postcssOptions;
-        },
-      };
-    },
-  ],
+  plugins: [tailwindPlugin],
 };
+
+async function tailwindPlugin(context, options) {
+  return {
+    name: 'docusaurus-tailwindcss-plugin',
+    configurePostCss(postcssOptions) {
+      // Appends TailwindCSS and AutoPrefixer.
+      postcssOptions.plugins.push(require('tailwindcss'));
+      postcssOptions.plugins.push(require('autoprefixer'));
+      return postcssOptions;
+    },
+  };
+}
 
 export default config;
