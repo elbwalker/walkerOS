77d12b813586a64c1a3e8033f0ff1721dcb262b5
alexander <Alexander@Kirtzel.com>
2023-11-24 11:44:42 +0100

    legal privacy page

---
+++ website/src/pages/legal/privacy.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import LegalPrivacyHeader from '@site/src/components/legal/privacy-header';
+import LegalPrivacyContent from '@site/src/components/legal/privacy-content';
+
+export default function LegalPrivacy(): JSX.Element {
+  const [language, changeLanguage] = React.useState<'EN' | 'DE'>('EN');
+
+  return (
+    <Layout title={`Privacy Policy`} description="elbwalker privacy policy">
+      <LegalPrivacyHeader changeLanguage={changeLanguage} />
+      <LegalPrivacyContent language={language} />
+    </Layout>
+  );
+}
