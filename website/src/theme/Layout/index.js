8e132048a801386bb19241cf8be9318f72b855d5
alexander <Alexander@Kirtzel.com>
2024-01-11 17:43:51 +0100

    version update and layout swizzle

---
+++ website/src/theme/Layout/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '@theme-original/Layout';
+
+export default function LayoutWrapper(props) {
+  return (
+    <>
+      <Layout {...props} />
+    </>
+  );
+}
