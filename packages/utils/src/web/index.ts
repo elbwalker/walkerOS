d3f3f16a784fb0eaa03e3259efd8fde4f6aeab47
alexander <Alexander@Kirtzel.com>
2024-01-16 14:53:24 +0100

    removed unused parseEvent

--- packages/utils/src/web/index.ts
+++ packages/utils/src/web/index.ts
@@ -1,5 +1,4 @@
 export { getAttribute } from './getAttribute';
 export { isVisible } from './isVisible';
-export { parseEvent } from './parseEvent';
 export { sessionStart } from './sessionStart';
 export { storageDelete, storageRead, storageWrite } from './storage';
