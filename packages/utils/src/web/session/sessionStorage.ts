c79e8513f658de305128b8fdfca1e2f320f611d0
alexander <Alexander@Kirtzel.com>
2024-01-30 18:41:07 +0100

    sessionStorage types

--- packages/utils/src/web/session/sessionStorage.ts
+++ packages/utils/src/web/session/sessionStorage.ts
@@ -1,16 +1,53 @@
-import { getId, getMarketingParameters } from '../../';
+import {
+  getId,
+  getMarketingParameters,
+  storageRead,
+  storageWrite,
+  tryCatch,
+} from '../../';
 import sessionStart from './sessionStart';
-import type { SessionStorage } from '.';
+import type { SessionStorageConfig, SessionStorageData } from '.';
 
 export default function sessionStorage(
-  config: SessionStorage = {},
+  config: SessionStorageConfig = {},
   utils: {
     getId: typeof getId;
     getMarketingParameters: typeof getMarketingParameters;
+    storageRead: typeof storageRead;
+    storageWrite: typeof storageWrite;
+    tryCatch: typeof tryCatch;
   },
-) {
-  // @TODO storage handling
-  config.isNew = true; // Dummy
+): SessionStorageData {
+  // Check for an existing session
+  let session: SessionStorageData | undefined = utils.tryCatch(
+    (key: string) => {
+      const existingSession: SessionStorageData = JSON.parse(
+        String(utils.storageRead(key)),
+      );
 
-  return sessionStart(config, utils);
+      // Check if session is still active
+
+      // Update sessions last activity
+
+      return existingSession;
+    },
+  )('session');
+
+  // Active session, nothing new
+  if (session) {
+    session.isNew = false;
+    return session;
+  }
+
+  // Create new session
+  config.isNew = true;
+  config.data = session;
+  session = Object.assign(sessionStart(config, utils) || {}, {
+    count: 1,
+    isNew: true,
+    id: utils.getId(12),
+    start: Date.now(),
+  });
+
+  return session;
 }
