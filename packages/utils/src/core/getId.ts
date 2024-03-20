4f645051b5506537805bf50dbf846998955c56af
alexander <Alexander@Kirtzel.com>
2024-03-20 16:52:58 +0100

    minify updates

--- packages/utils/src/core/getId.ts
+++ packages/utils/src/core/getId.ts
@@ -1,5 +1,6 @@
 export function getId(length = 6): string {
-  for (var str = '', l = 36; str.length < length; )
+  let str = '';
+  for (let l = 36; str.length < length; )
     str += ((Math.random() * l) | 0).toString(l);
   return str;
 }
