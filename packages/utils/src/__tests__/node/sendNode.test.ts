824abdd2ba7d769241603d3515092090ceb83005
alexander <Alexander@Kirtzel.com>
2024-07-08 12:44:33 +0200

    https test

--- packages/utils/src/__tests__/node/sendNode.test.ts
+++ packages/utils/src/__tests__/node/sendNode.test.ts
@@ -1,12 +1,15 @@
 import { sendRequestNode } from '../../';
 import * as http from 'http';
+import * as https from 'https';
 
 jest.mock('http');
+jest.mock('https');
 
 describe('sendRequestNode', () => {
   const data = { key: 'value' };
   const dataStringified = JSON.stringify({ key: 'value' });
-  const url = 'http://example.com/';
+  const urlHttp = 'http://example.com/';
+  const urlHttps = 'https://example.com/';
 
   const mockRequest = {
     on: jest.fn(),
@@ -39,13 +42,37 @@ describe('sendRequestNode', () => {
     };
 
     (http.request as jest.Mock).mockImplementation(mockFn);
+    (https.request as jest.Mock).mockImplementation(mockFn);
   });
 
   test('http request', async () => {
-    const response = await sendRequestNode(url, data);
+    const response = await sendRequestNode(urlHttp, data);
 
     expect(http.request).toHaveBeenCalledWith(
-      url,
+      urlHttp,
+      expect.objectContaining({
+        method: 'POST',
+        headers: expect.any(Object),
+      }),
+      expect.any(Function),
+    );
+
+    expect(mockRequest.write).toHaveBeenCalledWith(expect.any(String));
+
+    expect(response).toEqual({
+      ok: true,
+      response: dataStringified,
+      error: undefined,
+    });
+
+    expect(mockRequest.end).toHaveBeenCalled();
+  });
+
+  test('https request', async () => {
+    const response = await sendRequestNode(urlHttps, data);
+
+    expect(https.request).toHaveBeenCalledWith(
+      urlHttps,
       expect.objectContaining({
         method: 'POST',
         headers: expect.any(Object),
