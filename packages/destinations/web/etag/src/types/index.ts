769771d70a5ca40d0c855a871af12c2be3d70f63
alexander <Alexander@Kirtzel.com>
2024-07-26 11:26:40 +0200

    removed state

--- packages/destinations/web/etag/src/types/index.ts
+++ packages/destinations/web/etag/src/types/index.ts
@@ -1,4 +1,3 @@
-import type { WalkerOS } from '@elbwalker/types';
 import type { WebDestination } from '@elbwalker/walker.js';
 import type { DestinationCoreEtag } from '@elbwalker/destination-core-etag';
 
@@ -10,16 +9,3 @@ export type Config = WebDestination.Config<CustomConfig, CustomEventConfig>;
 export interface CustomConfig extends DestinationCoreEtag.Config {}
 
 export interface CustomEventConfig {}
-
-export interface State {
-  count?: number; // Sent events count
-  lastEngagement?: number; // Last event timestamp
-  isEngaged?: boolean; // If a user is engaged
-  sentPageView?: boolean; // If a page view has been sent
-  sentSession?: boolean; // If session parameters have been sent
-}
-
-export interface Context {
-  session?: WalkerOS.SessionData;
-  userAgent?: string;
-}
