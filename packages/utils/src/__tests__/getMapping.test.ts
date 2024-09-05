78513eac5662d2f1051f425e49e8c9ecf42fd603
alexander <Alexander@Kirtzel.com>
2024-09-05 19:45:57 +0200

    getMapping tests

---
+++ packages/utils/src/__tests__/getMapping.test.ts
@@ -0,0 +1,25 @@
+import type { WalkerOS } from '@elbwalker/types';
+import { getMappingValue } from '../';
+
+describe('getMappingValue', () => {
+  let event: WalkerOS.Event;
+
+  beforeEach(() => {
+    event = { data: { foo: 'bar' } } as unknown as WalkerOS.Event;
+  });
+
+  test('basic', () => {
+    expect(getMappingValue(event, 'data.foo')).toBe('bar');
+  });
+
+  test('key', () => {
+    expect(getMappingValue(event, { key: 'data.foo' })).toBe('bar');
+  });
+
+  test('default', () => {
+    expect(getMappingValue(event, { default: 'fallback' })).toBe('fallback');
+    expect(
+      getMappingValue(event, { key: 'data.nope', default: 'fallback' }),
+    ).toBe('fallback');
+  });
+});
