a2240c2ec4f8ecef8dad3ecd057682d8c8cacc0f
alexander <Alexander@Kirtzel.com>
2024-10-30 16:50:25 +0100

    code separation

--- packages/destinations/web/google-ga4/src/index.ts
+++ packages/destinations/web/google-ga4/src/index.ts
@@ -1,13 +1,6 @@
 import type { WalkerOS } from '@elbwalker/types';
-import type {
-  CustomConfig,
-  Destination,
-  GtagItems,
-  Include,
-  Parameters,
-  Params,
-} from './types';
-import { getMappingValue } from '@elbwalker/utils';
+import type { CustomConfig, Destination, Parameters } from './types';
+import { getParams, getParamsInclude, getParamsItems } from './parameters';
 
 // Types
 export * as DestinationGoogleGA4 from './types';
@@ -105,85 +98,6 @@ function addScript(
   const script = document.createElement('script');
   script.src = src + measurementId;
   document.head.appendChild(script);
-}
-
-function getParamsInclude(event: WalkerOS.Event, include: Include): Parameters {
-  const params: Parameters = {};
-
-  // Check for the 'all' group to add each group
-  if (include.includes('all'))
-    include = [
-      'context',
-      'data',
-      'event',
-      'globals',
-      'source',
-      'user',
-      'version',
-    ];
-
-  include.forEach((groupName) => {
-    let group = event[groupName as keyof Omit<WalkerOS.Event, 'all'>];
-
-    // Create a fake group for event properties
-    if (groupName == 'event')
-      group = {
-        id: event.id,
-        timing: event.timing,
-        trigger: event.trigger,
-        entity: event.entity,
-        action: event.action,
-        group: event.group,
-        count: event.count,
-      };
-
-    Object.entries(group).forEach(([key, val]) => {
-      // Different value access for context
-      if (groupName == 'context') val = (val as WalkerOS.OrderedProperties)[0];
-
-      params[`${groupName}_${key}`] = val;
-    });
-  });
-
-  return params;
-}
-
-function getParams(event: WalkerOS.Event, mapping: Params): Parameters {
-  const params = Object.entries(mapping).reduce((acc, [key, mapping]) => {
-    const value = getMappingValue(event, mapping);
-    if (value) acc[key] = value;
-    return acc;
-  }, {} as Parameters);
-
-  return params;
-}
-
-function getParamsItems(event: WalkerOS.Event, mapping: Params): Parameters {
-  let itemsCount = 0; // This will become the total items
-  const params = Object.entries(mapping).reduce((acc, [key, mapping]) => {
-    const value = getMappingValue(event, mapping);
-    if (value) {
-      itemsCount = itemsCount || 1;
-      // Define the number of items based on the longest array
-      if (Array.isArray(value) && value.length > itemsCount)
-        itemsCount = value.length;
-
-      acc[key] = value;
-    }
-
-    return acc;
-  }, {} as Parameters);
-
-  const items: GtagItems = [];
-  for (let i = 0; i < itemsCount; i++) {
-    const item = (items[i] = {} as Gtag.Item);
-    Object.entries(params).forEach(([key, paramValue]) => {
-      const value = Array.isArray(paramValue) ? paramValue[i] : paramValue;
-      if (value) item[key as keyof Gtag.Item] = value;
-    });
-  }
-
-  return items.length ? { items } : {};
 }
 
 export default destinationGoogleGA4;
