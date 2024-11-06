97a620b9ad495b54e3f172b825e5cc33b8ae69dd
alexander <Alexander@Kirtzel.com>
2024-11-06 16:13:44 +0100

    interception

--- packages/connectors/datalayer/src/index.ts
+++ packages/connectors/datalayer/src/index.ts
@@ -1,6 +1,6 @@
 import type { WalkerOS } from '@elbwalker/types';
 import type { Config, DataLayer } from './types';
-import { clone, tryCatch } from '@elbwalker/utils';
+import { interceptPush } from './intercept';
 
 export * as ConnectorDataLayer from './types';
 
@@ -16,28 +16,8 @@ export function elbDataLayer(push: WalkerOS.Elb, config: Config = {}) {
   // Store the original push function to preserve existing functionality
   const originalPush = dataLayer.push.bind(dataLayer);
 
-  dataLayer.push = function (...args: unknown[]) {
-    tryCatch((...args: unknown[]) => {
-      // Clone the arguments to avoid mutation
-      const clonedArgs = clone(args);
-
-      // Map the incoming event to a WalkerOS event
-      const event = mapPush(clonedArgs);
-
-      // Hand over to walker instance
-      push(event);
-    })(args);
-
-    // Always call the original push function
-    return originalPush(...args);
-  };
+  // Override the original push function to intercept incoming events
+  dataLayer.push = interceptPush(originalPush, push);
 }
 
 export default { elbDataLayer };
-
-function mapPush(...clonedArgs: unknown[]): WalkerOS.PartialEvent {
-  clonedArgs;
-
-  // @TODO dummy return
-  return { event: 'e a' };
-}
---
+++ packages/connectors/datalayer/src/mapping.ts
@@ -0,0 +1,8 @@
+import type { WalkerOS } from '@elbwalker/types';
+
+export function mapPush(...clonedArgs: unknown[]): WalkerOS.PartialEvent {
+  clonedArgs;
+
+  // @TODO dummy return
+  return { event: 'e a' };
+}
