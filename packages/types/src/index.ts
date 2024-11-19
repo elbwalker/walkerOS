dffebb49d5a0bd1d0e3b656cc131fff85aa49403
alexander <Alexander@Kirtzel.com>
2024-11-19 13:21:39 +0100

    hello Request

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -3,5 +3,6 @@ export * as Destination from './destination';
 export * as Handler from './handler';
 export * as Hooks from './hooks';
 export * as Mapping from './mapping';
+export * as Request from './request';
 export * as Schema from './schema';
 export * as WalkerOS from './walkeros';
---
+++ packages/types/src/request.ts
@@ -0,0 +1,10 @@
+export interface Context {
+  city?: string;
+  country?: string;
+  encoding?: string;
+  ip?: string;
+  language?: string;
+  origin?: string;
+  region?: string;
+  userAgent?: string;
+}
