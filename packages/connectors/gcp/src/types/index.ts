accc973a3f0da00edbff4a29fe0bd546c6040251
alexander <Alexander@Kirtzel.com>
2024-11-19 14:21:25 +0100

    org Request

--- packages/connectors/gcp/src/types/index.ts
+++ packages/connectors/gcp/src/types/index.ts
@@ -1,7 +1,4 @@
 import type { Request } from '@elbwalker/types';
-import type { Request as GCPRequest } from '@google-cloud/functions-framework';
-
-export type Request = GCPRequest;
 
 export interface HttpFunction {
   anonymizeIp?: boolean;
