50ef2516eac022db92e6dcfcc10e373ff366ee82
alexander <Alexander@Kirtzel.com>
2024-11-19 13:21:46 +0100

    header mapping

--- packages/connectors/gcp/src/__tests__/index.test.ts
+++ packages/connectors/gcp/src/__tests__/index.test.ts
@@ -1,9 +1,34 @@
+import type { Request as GCPRequest } from '@google-cloud/functions-framework';
 import { connectorGCPHttpFunction } from '..';
 
 describe('connector GCP', () => {
+  const request: GCPRequest = {
+    get: (header: string) => {
+      return {
+        origin: 'localhost',
+        'X-Real-Ip': '127.0.0.1',
+        'User-Agent': 'Mozilla/5.0',
+        'Accept-Language': 'ts',
+        'Accept-Encoding': 'gzip',
+        'X-AppEngine-Country': 'DE',
+        'X-AppEngine-Region': 'Hamburg',
+        'X-AppEngine-City': 'Hamburg',
+      }[header];
+    },
+  } as GCPRequest;
+
   beforeEach(() => {});
 
   test('init new', () => {
-    expect(connectorGCPHttpFunction).toBeDefined();
+    expect(connectorGCPHttpFunction(request)).toStrictEqual({
+      city: 'Hamburg',
+      country: 'DE',
+      encoding: 'gzip',
+      ip: '127.0.0.1',
+      language: 'ts',
+      origin: 'localhost',
+      region: 'Hamburg',
+      userAgent: 'Mozilla/5.0',
+    });
   });
 });
--- packages/connectors/gcp/src/index.ts
+++ packages/connectors/gcp/src/index.ts
@@ -1,8 +1,25 @@
-import type { Request } from '@google-cloud/functions-framework';
+import type { Request } from '@elbwalker/types';
+import type { Request as GCPRequest } from '@google-cloud/functions-framework';
 
 export * as ConnectorGCP from './types';
 
-export function connectorGCPHttpFunction(request: Request): undefined {
-  console.log('🚀 ~ request:', request);
-  return;
+export function connectorGCPHttpFunction(request: GCPRequest): Request.Context {
+  const context: Request.Context = {};
+  const headerMapping: Record<string, keyof typeof context> = {
+    origin: 'origin',
+    'X-Real-Ip': 'ip',
+    'User-Agent': 'userAgent',
+    'Accept-Language': 'language',
+    'Accept-Encoding': 'encoding',
+    'X-AppEngine-Country': 'country',
+    'X-AppEngine-Region': 'region',
+    'X-AppEngine-City': 'city',
+  };
+
+  Object.entries(headerMapping).forEach(([header, key]) => {
+    const value = request.get(header);
+    if (value) context[key] = value;
+  });
+
+  return context;
 }
