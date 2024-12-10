fe7dc1e43874403cf2ddd6bb9445d783fc5bdc70
alexander <Alexander@Kirtzel.com>
2024-12-10 23:37:50 +0100

    codeBox

---
+++ website/src/components/preview/codeBox.tsx
@@ -0,0 +1,66 @@
+import { Highlight, themes as prismThemes } from 'prism-react-renderer';
+import Editor from 'react-simple-code-editor';
+
+interface CodeBoxProps {
+  label: string;
+  value: string;
+  onChange?: (code: string) => void;
+  disabled?: boolean;
+  language?: string;
+}
+
+const CodeBox: React.FC<CodeBoxProps> = ({
+  label,
+  value,
+  onChange,
+  disabled = false,
+  language = 'javascript',
+}) => {
+  const highlightCode = (code: string) => (
+    <Highlight theme={prismThemes.palenight} code={code} language={language}>
+      {({ tokens, getLineProps, getTokenProps }) => (
+        <>
+          {tokens.map((line, i) => (
+            <div {...getLineProps({ line, key: i })} key={i}>
+              {line.map((token, key) => (
+                <span {...getTokenProps({ token, key })} key={key} />
+              ))}
+            </div>
+          ))}
+        </>
+      )}
+    </Highlight>
+  );
+
+  return (
+    <div className="w-1/3 border border-base-300 overflow-hidden flex flex-col">
+      <div className="border-b border-base-300 px-2 py-1 text-center">
+        {label}
+      </div>
+      <div className="flex-1 overflow-auto">
+        <Editor
+          value={value}
+          disabled={disabled}
+          onValueChange={onChange}
+          highlight={highlightCode}
+          padding={10}
+          style={{
+            fontFamily: '"Fira Code", monospace',
+            fontSize: 14,
+            backgroundColor: '#282c34',
+            color: '#abb2bf',
+            minHeight: '100%',
+            outline: 'none',
+            whiteSpace: 'pre', // Prevents line wrapping
+            overflowX: 'auto', // Enables horizontal scrolling
+            wordBreak: 'normal', // Prevents breaking words
+            overflowWrap: 'normal', // Prevents wrapping at all
+          }}
+          className="code-editor"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CodeBox;
--- website/src/components/preview/mapping.tsx
+++ website/src/components/preview/mapping.tsx
@@ -1,67 +1,12 @@
 import type { WalkerOS } from '@elbwalker/types';
-import React, { useEffect, useState, useRef, useCallback } from 'react';
-import { debounce, isObject } from '@elbwalker/utils';
+import { useEffect, useState, useRef, useCallback } from 'react';
+import { createEvent, debounce, isObject } from '@elbwalker/utils';
 import destinationGoogleGA4 from '@elbwalker/destination-web-google-ga4';
-import { Highlight, themes as prismThemes } from 'prism-react-renderer';
-import Editor from 'react-simple-code-editor';
+import CodeBox from './codeBox';
 
-interface CodeBoxProps {
-  label: string;
-  value: string;
-  onChange?: (code: string) => void;
-  disabled?: boolean;
-  language?: string;
-}
-
-const CodeBox: React.FC<CodeBoxProps> = ({
-  label,
-  value,
-  onChange,
-  disabled = false,
-  language = 'javascript',
-}) => {
-  const highlightCode = (code: string) => (
-    <Highlight theme={prismThemes.palenight} code={code} language={language}>
-      {({ tokens, getLineProps, getTokenProps }) => (
-        <>
-          {tokens.map((line, i) => (
-            <div {...getLineProps({ line, key: i })} key={i}>
-              {line.map((token, key) => (
-                <span {...getTokenProps({ token, key })} key={key} />
-              ))}
-            </div>
-          ))}
-        </>
-      )}
-    </Highlight>
-  );
-
-  return (
-    <div className="w-1/3 border border-base-300 overflow-hidden flex flex-col">
-      <div className="border-b border-base-300 px-2 py-1 text-center">
-        {label}
-      </div>
-      <div className="flex-1 overflow-auto">
-        <Editor
-          value={value}
-          disabled={disabled}
-          onValueChange={onChange}
-          highlight={highlightCode}
-          padding={10}
-          style={{
-            fontFamily: '"Fira Code", monospace',
-            fontSize: 14,
-            backgroundColor: '#282c34',
-            color: '#abb2bf',
-            minHeight: '100%',
-            outline: 'none',
-          }}
-          className="code-editor"
-        />
-      </div>
-    </div>
-  );
-};
+// @TODO
+// separate config and mapping
+// one init part at the beginning and use it as config and configure the mapping here
 
 interface MappingProps {
   event?: WalkerOS.AnyObject;
@@ -78,6 +23,7 @@ const Mapping: React.FC<MappingProps> = ({
   const [custom, setCustom] = useState(JSON.stringify(initCustom, null, 2));
   const [logs, setLogs] = useState<string[]>([]);
 
+  // @TODO make gtag function configurable
   const gtagFn = useCallback((...args: unknown[]) => {
     const params = args.map((arg) => {
       return isObject(arg) ? JSON.stringify(arg, null, 2) : `"${arg}"`;
@@ -85,12 +31,18 @@ const Mapping: React.FC<MappingProps> = ({
     setLogs([`gtag(${params.join(', ')})`]);
   }, []);
 
+  const parseJavaScriptObject = (code: string): unknown => {
+    return Function('"use strict"; return (' + code + ')')();
+  };
+
   const consoleLogRef = useRef(
     debounce((eventStr: string, customStr: string) => {
       setLogs([]);
+
       try {
-        const parsedEvent = JSON.parse(eventStr);
-        const parsedCustom = JSON.parse(customStr);
+        const parsedEvent = createEvent(parseJavaScriptObject(eventStr));
+        const parsedCustom = parseJavaScriptObject(customStr) as never;
+
         destinationGoogleGA4.push(parsedEvent, {
           custom: parsedCustom,
           init: true,
@@ -120,21 +72,10 @@ const Mapping: React.FC<MappingProps> = ({
 
         <CodeBox
           label="Result"
-          disabled={false}
+          disabled={true}
           value={logs[0] || 'No event yet.'}
         />
       </div>
-
-      <style>
-        {`
-          .code-editor {
-            outline: none;
-          }
-          pre {
-            margin: 0;
-          }
-        `}
-      </style>
     </div>
   );
 };
