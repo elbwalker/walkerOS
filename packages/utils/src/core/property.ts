c5463eb1a43e5235a68c02721c8a3997d61f7b9d
alexander <Alexander@Kirtzel.com>
2024-12-17 16:04:51 +0100

    is functions

--- packages/utils/src/core/property.ts
+++ packages/utils/src/core/property.ts
@@ -1,16 +1,45 @@
 import type { WalkerOS } from '@elbwalker/types';
+import { isArguments, isArray, isNumber, isObject } from './is';
 
 export function isPropertyType(value: unknown): value is WalkerOS.PropertyType {
   return (
     typeof value === 'boolean' ||
     typeof value === 'string' ||
-    typeof value === 'number' ||
+    isNumber(value) ||
     typeof value === 'undefined' ||
-    (Array.isArray(value) && value.every(isPropertyType)) ||
-    (typeof value === 'object' &&
-      value !== null &&
-      Object.values(value).every(isPropertyType))
+    (isArray(value) && value.every(isPropertyType)) ||
+    (isObject(value) && Object.values(value).every(isPropertyType))
   );
+}
+
+export function filterValues(value: unknown): WalkerOS.Property | undefined {
+  if (
+    typeof value === 'boolean' ||
+    typeof value === 'string' ||
+    isNumber(value)
+  )
+    return value;
+
+  if (isArguments(value)) return filterValues(Array.from(value));
+
+  if (isArray(value)) {
+    return value
+      .map((item) => filterValues(item))
+      .filter((item): item is WalkerOS.PropertyType => item !== undefined);
+  }
+
+  if (isObject(value)) {
+    return Object.entries(value).reduce<Record<string, WalkerOS.Property>>(
+      (acc, [key, val]) => {
+        const filteredValue = filterValues(val);
+        if (filteredValue !== undefined) acc[key] = filteredValue;
+        return acc;
+      },
+      {},
+    );
+  }
+
+  return;
 }
 
 export function castToProperty(value: unknown): WalkerOS.Property | undefined {
