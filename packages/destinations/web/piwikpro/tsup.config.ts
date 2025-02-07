0fb0f0d7f85f6f6395ed738a64b7fa7b9839aaff
alexander <Alexander@Kirtzel.com>
2025-02-07 11:01:30 +0100

    web destination tsup config

--- packages/destinations/web/piwikpro/tsup.config.ts
+++ packages/destinations/web/piwikpro/tsup.config.ts
@@ -1,38 +1,16 @@
-import { config, defineConfig } from '@elbwalker/tsup';
+import {
+  defineConfig,
+  buildModules,
+  buildExamples,
+  buildBrowser,
+  buildES5,
+} from '@elbwalker/tsup';
 
 const globalName = 'Destination';
 
 export default defineConfig([
-  // CJS
-  {
-    ...config,
-    dts: true,
-    format: ['cjs'],
-    sourcemap: true,
-  },
-  // Module mjs
-  {
-    ...config,
-    noExternal: [/(.*)/],
-    format: ['esm'],
-  },
-  // Browser
-  {
-    ...config,
-    format: ['iife'],
-    globalName,
-    outExtension() {
-      return { js: `.browser.js` };
-    },
-  },
-  // ES5
-  {
-    ...config,
-    format: ['iife'],
-    globalName,
-    outExtension() {
-      return { js: `.es5.js` };
-    },
-    target: 'es5',
-  },
+  buildModules(),
+  buildExamples(),
+  buildBrowser({ globalName }),
+  buildES5({ globalName }),
 ]);
