8008114ad1d5e74249c3a6654b05cc68a9cb7030
alexander <Alexander@Kirtzel.com>
2025-02-10 10:47:50 +0100

    jest update

--- packages/tagger/tsup.config.ts
+++ packages/tagger/tsup.config.ts
@@ -1,27 +1,16 @@
-import { Options, defineConfig } from 'tsup';
-
-const config: Options = {
-  // clean: true, // Not yet supported for multiple entry points
-  entry: ['src/index.ts'],
-  minify: 'terser',
-  splitting: false,
-  terserOptions: {
-    mangle: {
-      properties: {
-        regex: /^[A-Z]/, // Only mangle capitalized properties
-        reserved: [
-          // Prevent mangle from renaming these properties
-        ],
-      },
-    },
-  },
-};
+import { defineConfig, buildModules } from '@elbwalker/tsup';
 
 export default defineConfig([
-  {
-    ...config,
-    dts: true,
-    format: ['cjs', 'esm'],
-    sourcemap: true,
-  },
+  buildModules({
+    terserOptions: {
+      mangle: {
+        properties: {
+          regex: /^[A-Z]/, // Only mangle capitalized properties
+          reserved: [
+            // Prevent mangle from renaming these properties
+          ],
+        },
+      },
+    },
+  }),
 ]);
