b2016676c78545ca1eca2e3d9263423720338a23
aylaju <99686309+aylaju@users.noreply.github.com>
2025-03-31 13:47:33 +0200

    demo

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -3,9 +3,13 @@ import Layout from '@theme/Layout';
 import Hero from '@site/src/components/home/hero';
 import Features from '@site/src/components/home/features';
 import CTAStart from '@site/src/components/ctas/start';
+<<<<<<< Updated upstream
 import PromotionLive, {
   PromotionReady,
 } from '@site/src/components/templates/promotionLive';
+=======
+import PromotionLive from '../components/templates/promotionLive';
+>>>>>>> Stashed changes
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
@@ -15,7 +19,11 @@ export default function Home() {
       description="Open-source event data collection platform"
     >
       <Hero />
+<<<<<<< Updated upstream
       <PromotionReady />
+=======
+      <PromotionLive />
+>>>>>>> Stashed changes
       <main>
         <Features />
         <PromotionLive />
