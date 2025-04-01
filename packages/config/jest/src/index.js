fe798160ac38b2bd4827719b5070110d2af8ca6e
alexander <Alexander@Kirtzel.com>
2025-04-01 20:02:27 +0200

    jest watch mode code

--- packages/config/jest/src/index.js
+++ packages/config/jest/src/index.js
@@ -1,26 +1,30 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-function getModuleMapper() {
-  const packagesDir = path.join(
-    path.dirname(fileURLToPath(import.meta.url)),
-    '..',
-    '..',
-    '/',
-  );
+const isWatchMode = process.argv.includes('--watchAll');
+const packagesDir = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  '..',
+  '..',
+  '..',
+  '/',
+);
 
-  function getDirectory(dir) {
-    return path.join(packagesDir, dir, 'src');
-  }
+function getDirectory(dir) {
+  return path.join(packagesDir, dir);
+}
+
+function getModuleMapper() {
+  if (!isWatchMode) return {};
 
   return {
-    // '^@elbwalker/jest$1': getDirectory('config/jest$1'),
-    // '^@elbwalker/types': getDirectory('types'),
-    // '^@elbwalker/utils$1': getDirectory('utils$1'),
-    // '^@elbwalker/walker.js': getDirectory('sources/walkerjs'),
-    // '^@elbwalker/destination-web-(.*)$': getDirectory('destinations/web/$1'),
-    // '^@elbwalker/destination-node-(.*)$': getDirectory('destinations/node/$1'),
-    '^@elbwalker/source-(.*)$': getDirectory('sources/$1'),
+    '^@elbwalker/utils$': getDirectory('utils/src/'),
+    '^@elbwalker/utils/node$': getDirectory('utils/src/node/'),
+    '^@elbwalker/utils/web$': getDirectory('utils/src/web/'),
+    '^@elbwalker/walker.js$': getDirectory('sources/walkerjs'),
+    '^@elbwalker/source-node$': getDirectory('sources/node'),
+    '^@elbwalker/destination-web-(.*)$': getDirectory('destinations/web/$1'),
+    '^@elbwalker/destination-node-(.*)$': getDirectory('destinations/node/$1'),
   };
 }
 
@@ -45,7 +49,7 @@ const config = {
   transformIgnorePatterns: ['/node_modules/(?!(@elbwalker)/)'],
   testMatch: ['<rootDir>/**/*.test.(ts|tsx|js|jsx)'],
   moduleFileExtensions: ['js', 'ts', 'mjs'],
-  rootDir: 'src',
+  rootDir: '.',
   moduleDirectories: ['node_modules', 'src'],
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   moduleNameMapper: getModuleMapper(),
