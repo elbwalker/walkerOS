bcb90637a4b0e49b6a3a09d8e81ac8db41eb509e
alexander <Alexander@Kirtzel.com>
2025-05-07 14:01:14 +0200

    Purchase mapping

--- packages/destinations/node/meta/src/examples/events.ts
+++ packages/destinations/node/meta/src/examples/events.ts
@@ -1,6 +1,7 @@
+import type { BodyParameters } from '../types';
 import { getEvent } from '@elbwalker/utils';
 
-export function Purchase() {
+export function Purchase(): BodyParameters {
   const event = getEvent('order complete');
 
   return {
@@ -12,6 +13,15 @@ export function Purchase() {
         event_source_url: event.source.id,
         action_source: 'website',
         user_data: {},
+        num_items: 2,
+        order_id: String(event.data.id),
+        contents: event.nested
+          .filter((item) => item.type === 'product')
+          .map((item) => ({
+            id: String(item.data.id),
+            quantity: Number(item.data.quantity) || 1,
+            item_price: Number(item.data.price),
+          })),
       },
     ],
   };
--- packages/destinations/node/meta/src/examples/mapping.ts
+++ packages/destinations/node/meta/src/examples/mapping.ts
@@ -1,5 +1,6 @@
 import type { Mapping, WalkerOS } from '@elbwalker/types';
 import type { DestinationMeta } from '..';
+import { isObject } from '@elbwalker/utils';
 
 export const InitUserData: DestinationMeta.Custom = {
   pixelId: 'p1x3l1d',
@@ -13,7 +14,27 @@ export const Purchase: DestinationMeta.EventConfig = {
   name: 'Purchase',
   data: {
     map: {
-      event_id: 'id',
+      order_id: 'data.id',
+      contents: {
+        loop: [
+          'nested',
+          {
+            condition: (entity) =>
+              isObject(entity) && entity.type === 'product',
+            map: {
+              id: 'data.id',
+              item_price: 'data.price',
+              quantity: { key: 'data.quantity', value: 1 },
+            },
+          },
+        ],
+      },
+      num_items: {
+        fn: (event) =>
+          (event as WalkerOS.Event).nested.filter(
+            (item) => item.type === 'product',
+          ).length,
+      },
     },
   },
 };
