01501a1c3e80ea66a7bda137ef739796761b7755
alexander <Alexander@Kirtzel.com>
2025-09-06 14:27:19 +0200

    more type to entity

--- packages/server/destinations/gcp/src/bigquery/__tests__/index.test.ts
+++ packages/server/destinations/gcp/src/bigquery/__tests__/index.test.ts
@@ -88,7 +88,7 @@ describe('Server Destination BigQuery', () => {
         custom: '{"completely":"random"}',
         user: '{"id":"us3r","device":"c00k13","session":"s3ss10n"}',
         nested:
-          '[{"type":"child","data":{"is":"subordinated"},"nested":[],"context":{"element":["child",0]}}]',
+          '[{"entity":"child","data":{"is":"subordinated"},"nested":[],"context":{"element":["child",0]}}]',
         trigger: 'test',
         timing: 3.14,
         group: 'gr0up',
