f20278668ec061c12af0865a9e5a925d91b39bcf
alexander <Alexander@Kirtzel.com>
2025-09-23 17:40:40 +0200

    bye generator

--- packages/generator/jest.config.mjs
+++
@@ -1,9 +0,0 @@
-import baseConfig from '@walkeros/jest';
-
-export default {
-  ...baseConfig,
-  displayName: '@walkeros/generator',
-  testTimeout: 30000, // 30 second timeout for tests that involve npm operations
-  detectOpenHandles: true, // Detect hanging promises/handles
-  testMatch: ['<rootDir>/src/__tests__/**/*.test.ts'], // Integration and CLI tests
-};
