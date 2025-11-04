40ea440dd1f2f55dd916df5ad3e2471a5d95dab2
alexander <Alexander@Kirtzel.com>
2025-11-04 21:05:32 +0100

    MDXComponents

--- website/src/theme/MDXComponents.js
+++ website/src/theme/MDXComponents.js
@@ -1,10 +1,28 @@
-import React from 'react';
 // Import the original mapper
 import MDXComponents from '@theme-original/MDXComponents';
+import Link from '@docusaurus/Link';
 import { Icon } from '@iconify/react';
+// Import explorer components (made available in all MDX files)
+import {
+  CodeBox,
+  PropertyTable,
+  DestinationInitDemo,
+  DestinationDemo,
+} from '@walkeros/explorer';
+// Import website-specific components
+import PackageLink from '@site/src/components/docs/package-link';
 
 export default {
   // Re-use the default mapping
   ...MDXComponents,
+  // Docusaurus components
+  Link,
+  // Custom components
   Icon,
+  PackageLink,
+  // Explorer components (no import needed in MDX files)
+  CodeBox,
+  PropertyTable,
+  DestinationInitDemo,
+  DestinationDemo,
 };
