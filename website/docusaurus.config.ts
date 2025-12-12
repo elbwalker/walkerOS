fd949a920533222d685427d942be3ed77936371b
alexander <Alexander@Kirtzel.com>
2025-12-12 18:31:05 +0100

    docusaurus baseurl

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   url: 'https://www.walkeros.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: process.env.DOCUSAURUS_BASEURL || '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
