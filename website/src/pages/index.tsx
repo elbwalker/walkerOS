7f1ec396a9f2c7d8a047094a2d6bb50df439352d
aylaju <ayla@elbwalker.com>
2026-04-14 13:28:02 +0200

    Update index.tsx

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -4,7 +4,6 @@ import Features from '@site/src/components/organisms/features';
 import Integrations from '@site/src/components/home/integrations';
 import GettingStarted from '@site/src/components/organisms/gettingStarted';
 import Benefits from '@site/src/components/organisms/benefits';
-import Vision from '@site/src/components/home/vision';
 import CTAStart from '@site/src/components/ctas/start';
 
 export default function Home() {
@@ -19,7 +18,6 @@ export default function Home() {
         <Features />
         <Integrations />
         <Benefits />
-        <Vision />
         <CTAStart />
       </main>
     </Layout>
